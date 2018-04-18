4e4cbc8415db36695035a0f9255e30687ebb0846
松原健太 <m2ken@matsubarakentanoMacBook-Pro.local>
2018-04-18 13:00:09 +0900

    push

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,10 @@ class BooksController < ApplicationController
   def create
 	  	book = Book.new(book_params)
 	  	book.save
-	  	redirect_to 'books'
+	  	redirect_to books_path
   end
+
+  
 
   def edit
   		@book = Book.find(params[:id])
